0b1fccc19ec3c8e56cba93266ccc815877159c01
Shahar_Nahum <62279310+shahar-nahum90@users.noreply.github.com>
2020-05-06 10:16:23 +0300

    Add files via upload

---
+++ Car.cs
@@ -0,0 +1,44 @@
+﻿using MiNET.Effects;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Class_Task_05_Q2
+{
+    class Car
+    {
+        private int speed;
+
+        public int Speed
+        {
+            get { return speed; }
+            set { speed = value; }
+        }
+
+
+        public void AccelerationSpeed(int num)
+        {
+            while (num > 0)
+            {
+                Speed++;
+                num--;
+            }
+        }
+        public void LoweringSpeed(int num)
+        {
+            while (num > 0)
+            {
+                Speed--;
+                num--;
+            }
+        }
+        public void StopCar()
+        {
+            Speed = 0;
+        }
+        public string GetDescription()
+        {
+            return $" The speed of your car: {Speed}";
+        }
+    }
+}
