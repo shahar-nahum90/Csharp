12a48ea676ed398131b5e2f5538e7a3518c6b58f
Shahar_Nahum <62279310+shahar-nahum90@users.noreply.github.com>
2020-05-06 10:14:42 +0300

    Add files via upload

---
+++ Cookie.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Lesson_05
+{
+    class Cookie
+    {
+        private Boolean gloten;
+        public Boolean Gloten
+        {
+            get { return gloten; }
+            set { gloten = value; }
+        }
+
+        private int numOfEggs;
+        public int NumOfEggs
+        {
+            get { return numOfEggs; }
+            set
+            {
+                if (value > 0)
+                    numOfEggs = value;
+            }
+        }
+
+        private float numOfSuger;
+        public float NumOfSuger
+        {
+            get { return numOfSuger; }
+            set { numOfSuger = value; }
+        }
+
+        private float amountflour;
+        public float Amountflour
+        {
+            get { return amountflour; }
+            set { amountflour = value; }
+        }
+
+    }
+}
