5fd573308e88920a583c32b6aac068a3b7a6358c
Shahar_Nahum <62279310+shahar-nahum90@users.noreply.github.com>
2020-05-06 10:15:51 +0300

    Add files via upload

---
+++ MainClassCar.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Class_Task_05_Q2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car1 = new Car();
+            Console.WriteLine(car1.Speed);
+
+            Console.WriteLine("Do you want to increase speed? If so, how many?");
+
+            car1.AccelerationSpeed(Convert.ToInt32(Console.ReadLine()));
+
+            Console.WriteLine(car1.Speed);
+
+            car1.LoweringSpeed(2);
+            Console.WriteLine(car1.Speed);
+
+            car1.StopCar();
+            Console.WriteLine(car1.Speed);
+        }
+    }
+}
